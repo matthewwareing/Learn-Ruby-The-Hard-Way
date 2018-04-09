880250fc42ebe08f4edadd82741fa4b8185cdb04
Matt <mdwareing@gmail.com>
2018-04-09 13:30:10 +0100

    First 21 exercises

---
+++ ex5.rb
@@ -0,0 +1,15 @@
+name = 'Matthew Wareing'
+age = 22
+height = 72 # inches
+weight = 76 # kilograms
+eyes = 'Hazel'
+teeth = 'White'
+hair = 'Brown'
+
+puts "Let's talk about #{name}."
+puts "He's #{height} inches tall."
+puts "He's #{weight} kilograms heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth} depending on the coffee."
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
